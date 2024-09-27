a28b6a2441dd15fa2c37ff4841f4b2a3daba898f
DalgatAhi <156659789+DalgatAhi@users.noreply.github.com>
2024-09-27 21:35:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 let tabBtns = document.querySelectorAll('.tab__btn')
 let tabTexts = document.querySelectorAll('.tab__text')
 
-tabBtns.forEach((item, index) => {
-    item.addEventListener('click', function () {
+tabBtns.forEach((item, index)=>{
+    item.addEventListener('click', function(){
         document.querySelector('.tab__btn.active').classList.remove('active');
         item.classList.add('active');
 
@@ -12,15 +12,3 @@ tabBtns.forEach((item, index) => {
         tabTexts[index].classList.add('active')
     })
 })
-
-function sayHi() {
-    alert('hi')
-}
-let timerId;
-start.onclick = function () {
-    timerId = setTimeout(sayHi, 3000)
-}
-cancel.onclick = function () {
-    clearTimeout(timerId)
-    alert(`Отмена таймера с id: ${timerId}`)
-}
